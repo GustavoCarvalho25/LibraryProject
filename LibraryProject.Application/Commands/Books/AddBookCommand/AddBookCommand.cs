aa05d3e074c4f963988e8d6dd91c2f82f20bca12
GustavoCarvalho25 <gustavo.carvalhoas@hotmail.com>
2025-07-03 23:07:07 -0300

    fix(Book) - @GustavoCarvalho25: Corrigindo propriedades

--- LibraryProject.Application/Commands/Books/AddBookCommand/AddBookCommand.cs
+++ LibraryProject.Application/Commands/Books/AddBookCommand/AddBookCommand.cs
@@ -6,15 +6,13 @@ namespace Application.Commands.Books.AddBookCommand;
 public class AddBookCommand : IRequest<ResultViewModel<BookViewModel>>
 {
     public string Title { get; set; }
-    public string Description { get; set; }
     public string Author { get; set; }
     public string ISBN { get; set; }
     public int PublicationYear { get; set; }
     
-    public AddBookCommand(string title, string description, string author, string isbn, int publicationYear)
+    public AddBookCommand(string title, string author, string isbn, int publicationYear)
     {
         Title = title;
-        Description = description;
         Author = author;
         ISBN = isbn;
         PublicationYear = publicationYear;
--- LibraryProject.Application/ViewModels/BookViewModel.cs
+++ LibraryProject.Application/ViewModels/BookViewModel.cs
@@ -7,18 +7,18 @@ public class BookViewModel
     public Guid Id { get; private set; }
     public string Title { get; private set; }
     public string Author { get; private set; }
-    public string Description { get; set; }
     public string ISBN { get; private set; }
     public int PublicationYear { get; private set; }
+    public bool IsAvailable { get; set; } = true;
 
-    public BookViewModel(Guid id, string title, string author, string isbn, int publicationYear, string description)
+    public BookViewModel(Guid id, string title, string author, string isbn, int publicationYear, bool isAvailable)
     {
         Id = id;
         Title = title;
         Author = author;
         ISBN = isbn;
         PublicationYear = publicationYear;
-        Description = description;
+        IsAvailable = isAvailable;
     }
     
     public BookViewModel() {}
